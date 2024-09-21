9ed380e42244b095dd585ea87216389a1e0c4080
rendonir21@gmail.com <rendonir21@gmail.com>
2024-09-21 11:16:37 -0700

    updated frontend

--- frontend/melon-tasting-frontend/src/App.jsx
+++ frontend/melon-tasting-frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Register from './components/Register.jsx';
 import Login from './components/Login.jsx';
 import Reservations from './components/Reservations.jsx';
 import MyReservations from './components/MyReservations.jsx';
+import Homepage from './components/Homepage.jsx';
 
 export default function App() {
   const [userId, setUserId] = useState(null);  // Store userId in state
@@ -11,16 +12,14 @@ export default function App() {
   return (
     <Router>
       <div>
-        {!userId ? (
-          <Login setUserId={setUserId} />
-        ) : (
-          <Reservations userId={userId} />
-        )}
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/login" element={<Login setUserId={setUserId} />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/reservations" element={userId ? <Reservations userId={userId} /> : <Login setUserId={setUserId} />} />
+          <Route path="/my-reservations" element={userId ? <MyReservations userId={userId} /> : <Login setUserId={setUserId} />} />
+        </Routes>
       </div>
-      <Routes>
-       <Route path="/" element={<Register />} />
-        <Route path="/my-reservations" element={<MyReservations userId={userId} />} />
-      </Routes>
     </Router>
   );
 }
